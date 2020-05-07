9e064e8863555d097f615ba5bd42c5b652d0ff1b
Frank <franklinewax@gmail.com>
2020-05-07 07:33:03 +0300

    Initialising culculater project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  let number1=parseInt(prompt("Enter Number one"));
+  let number2=parseInt(prompt("Enter Number two"));
+  alert(add(number1,number2));
+  
